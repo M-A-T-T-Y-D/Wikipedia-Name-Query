21ab39b555c260affed6ce7dc63f3b6bb91c1b05
Matthew <Matt@Pauls-MacBook-Pro.local>
2024-04-18 13:59:39 +0100

    fixed unit test due to it failing

--- tests/test_querys.py
+++ tests/test_querys.py
@@ -10,10 +10,10 @@ def query_test1():
     query_test = Query()
     output = query_test.get_person_info("Donald Knuth")
     return output
-def test_full_name(query_test):
-    assert query_test[0][0] == "Donald Knuth"
-    assert query_test[0][1] == "1938-01-10"
-    assert query_test[0][2] is None
+def test_full_name(query_test1):
+    assert query_test1[0][0] == "Donald Knuth"
+    assert query_test1[0][1] == "1938-01-10"
+    assert query_test1[0][2] is None
 
 def test_rndm():
     query_test = Query()
@@ -22,5 +22,5 @@ def test_rndm():
 
 def test_none():
     query_test = Query()
-    with pytest.raises(ValueError) as excinfo:
+    with pytest.raises(ValueError):
         query_test.get_person_info(None)
